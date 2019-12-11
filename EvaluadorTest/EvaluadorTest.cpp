6396ae1698583eaddc3880dcc5030a0222c1b1e9
RAMIRO MIRO CABALLERO CASTELLANOS <www.joelramiro@unitec.edu>
2019-12-11 01:23:54 -0600

    Se agrego menu

--- EvaluadorTest/EvaluadorTest.cpp
+++ EvaluadorTest/EvaluadorTest.cpp
@@ -13,8 +13,40 @@ int main()
 	ExpressionValidation* expressionValidation = new ExpressionValidation();
 	ConversionAPostFija* conversionAPostFija = new ConversionAPostFija();
 	PostFijaOperacion* postFijaOperacion = new PostFijaOperacion();
-	auto r = expressionValidation->Validation("[(2+5)");
-	cout << postFijaOperacion->Operate(conversionAPostFija->Convert("[(2+5)")) << endl;
+	bool cont = true;
+	string xpression;
+	int o;
+	while (cont)
+	{
+		cout << "Ingrese expresion" << endl;
+		cin >> xpression;
+		//xpression = ReplaceAll(xpression, std::string(" "), std::string(""));
+		/*xpressionstd::replace(xpression.begin(), xpression.end(), ' ', '');*/
+		auto r = expressionValidation->Validation(xpression);
+		if (r)
+		{
+			cout << postFijaOperacion->Operate(conversionAPostFija->Convert(xpression)) << endl;
+		}
+		else
+		{
+			cout << "Expresion incorrecta" << endl;
+		}
+
+		cout << "Desea continuar : 1. Si" << endl;
+		cin >> o;
+		switch (o)
+		{
+		case 1:
+		{
+		}
+		break;
+		default:
+		{
+			cont = false;
+		}
+			break;
+		}
+	}
 	//cout<<postFijaOperacion->Operate(conversionAPostFija->Convert("10+(1+2)*2+2/2"))<<endl;
 	//cout << conversionAPostFija->HaveBadValues("10+2*5/[4]");
 }
